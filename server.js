1c8530e11812e9b5ab8b23871e730010a21f5e51
Gershom Maes <gershom.maes@genesys.com>
2020-09-11 15:32:43 -0400

    No more need to stop server to add assets (just modify assets.json at runtime!) Nicer splash page

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
 let [ http, fs, path ] = [ 'http', 'fs', 'path' ].map(require);
-let [ host, port ] = process.argv[2].split(':');
+let [ host, port ] = (process.argv[2] || 'localhost:80').split(':');
 
 (async () => {
   
-  let serve = (res, content, type) => {
-    res.writeHead(200, {
-      'Content-Type': type,
-      'Content-Length': Buffer.byteLength(content)
-    });
+  let serve = (res, status, content, type) => {
+    res.writeHead(status, { 'Content-Type': type, 'Content-Length': Buffer.byteLength(content) });
     res.end(content);
   };
-  let responses = {
-    icon: async res => serve(res, await fs.promises.readFile(path.join(__dirname, 'asset', 'image', 'favicon.ico')), 'image/x-icon'),
-    html: async res => serve(res, await fs.promises.readFile(path.join(__dirname, 'main.html')), 'text/html'),
-    css: async res => serve(res, await fs.promises.readFile(path.join(__dirname, 'main.css')), 'text/css'),
-    js: async res => serve(res, await fs.promises.readFile(path.join(__dirname, 'main.js')), 'text/javascript'),
-    bg: async res => serve(res, await fs.promises.readFile(path.join(__dirname, 'asset', 'image', 'bg.png')), 'image/png')
-  };
   
-  http.createServer(async (req, res) => console.log(req.url) || (responses[req.url.slice(1)] || responses.html)(res)).listen(port, host);
+  http.createServer(async (req, res) => {
+    
+    let reqMsg = [];
+    let addReqMsg = ln => reqMsg.push(...ln.split('\n'));
+    let serveReq = (...args) => {
+      console.log(`${req.connection.remoteAddress} <- ${req.url}${reqMsg.length ? ':' : ''}`);
+      if (reqMsg.length) console.log(reqMsg.map(ln => `> ${ln}`).join('\n'));
+      console.log('');
+      serve(...args);
+    };
+    
+    try {
+      
+      let servables = JSON.parse(await fs.promises.readFile(path.join(__dirname, 'assets.json')));
+      let [ contentType=null, ...fp ] = servables[req.url.slice(1)] || [];
+      if (!contentType) throw new Error(`Unknown asset: ${req.url}`);
+      serveReq(res, 200, await fs.promises.readFile(path.join(__dirname, ...fp)), contentType);
+      
+    } catch(err) {
+      
+      addReqMsg(`Error occurred: ${err.stack}`);
+      serveReq(res, 400, 'Your request is as invalid as the assertion that chicken is fleishig', 'text/plain');
+      
+    }
+    
+  }).listen(port, host);
   console.log(`Listening on ${host}:${port}`);
   
 })();
